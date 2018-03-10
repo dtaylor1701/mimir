0be7fd4329ffd463a8228c613289e463a51e6715
David Kincheloe Taylor <dktaylor1701@gmail.com>
2018-03-10 16:01:22 -0500

    Activities and emotions can now be removed

--- Mimir/Mimir/ActivitesViewController.swift
+++ Mimir/Mimir/ActivitesViewController.swift
@@ -62,17 +62,18 @@ class ActivitesViewController: UIViewController, UITableViewDelegate, UITableVie
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            mimir.user.activities.remove(at: indexPath.row)
+            Store.storeActivities(activties: mimir.user.activities)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
--- Mimir/Mimir/EmotionsViewController.swift
+++ Mimir/Mimir/EmotionsViewController.swift
@@ -62,17 +62,18 @@ class EmotionsViewController: UIViewController, UITableViewDelegate, UITableView
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            mimir.interpreter.emotions.remove(at: indexPath.row)
+            Store.storeEmotions(emotions: mimir.interpreter.emotions)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
