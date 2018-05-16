4b0bc9d3b900d88e2ffd7d5fa08ab81a88bdf906
David Kincheloe Taylor <dktaylor1701@gmail.com>
2018-05-16 18:11:44 -0400

    Added source files

---
+++ Sources/Models/Emotion.swift
@@ -0,0 +1,20 @@
+//
+//  Emotion.swift
+//  DecisionEngine
+//
+//  Created by David Taylor on 3/4/18.
+//  Copyright © 2018 Hyper Elephant. All rights reserved.
+//
+
+import Foundation
+
+public class Emotion : Codable {
+    public var name: String
+    /// The feeling of being this emotion
+    public var feel: Feel
+    
+    public init(name: String, feel: Feel) {
+        self.name = name
+        self.feel = feel
+    }
+}
