1834f5321f78824f1de9b422627c92efa1473816
David Taylor <dktaylor1701@gmail.com>
2018-05-16 18:00:29 -0400

    Initial structure

---
+++ Sources/Mimir.swift
@@ -0,0 +1,9 @@
+//
+//  Mimir.swift
+//  Mimir
+//
+//  Created by David Taylor on 5/16/18.
+//  Copyright © 2018 Mimir. All rights reserved.
+//
+
+import Foundation
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,6 @@
+import XCTest
+@testable import MimirTests
+
+XCTMain([
+    testCase(MimirTests.allTests),
+])
